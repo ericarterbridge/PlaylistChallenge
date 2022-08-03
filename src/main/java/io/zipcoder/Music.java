3bcca3b20d6c15eae8bab8c7617ae6e52d310fd5
eric <ericarterbridge@gmail.com>
2022-08-02 20:55:38 -0400

    completed lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +11,32 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int selectionIndex = 0;
+        int selectionIndex2 = 0;
+        int index2 = 0;
+        for (int i = 0; i < playList.length; i++){
+            if (playList[i].equalsIgnoreCase(selection)){
+                selectionIndex = i;
+            }
+        }
+        for (int i = 0; i < selectionIndex; i++){
+            if (playList[i].equalsIgnoreCase(selection)){
+                selectionIndex2 = i;
+                index2 = Math.abs(selectionIndex2 - startIndex);
+            } else index2 = Integer.MAX_VALUE;
+        }
+        int index1 = Math.abs(selectionIndex - startIndex);
+        int revIndex1 = startIndex + (playList.length - selectionIndex);
+        int revIndex2 = startIndex + (playList.length - selectionIndex2);
+        int[] indexArr = new int[4];
+        indexArr[0] = index1;
+        indexArr[1] = index2;
+        indexArr[2] = revIndex1;
+        indexArr[3] = revIndex2;
+        Arrays.sort(indexArr);
+
+        if (index1 < index2 && index1 < revIndex1 && index1 < revIndex2){
+        }
+        return indexArr[0];
     }
 }
